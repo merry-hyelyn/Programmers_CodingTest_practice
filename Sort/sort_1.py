79eaed81d35e4307130b39a389743e74b66dd18f
merryn <h1335@naver.com>
2020-04-03 18:13:31 +0900

    reload

--- Sort/sort_1.py
+++ Sort/sort_1.py
@@ -1,18 +1,3 @@
-# def solution(array, commands):
-#     answer = []
-#     for h in range(len(commands)):
-#         i = commands[h][0]
-#         j = commands[h][1]
-#         k = commands[h][2]
-#         a = array[i - 1: j]
-#         a.sort()
-#         answer.append(a[k - 1])
-#     return answer
-
-
-# array = [1, 5, 2, 6, 3, 7, 4]
-# commands = [[2, 5, 3], [4, 4, 1], [1, 7, 3]]
-# print(solution(array, commands))
 def solution(array, commands):
     answer = []
     for command in commands:
