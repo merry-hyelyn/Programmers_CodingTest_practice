8f9e6e1542a52117fd343ad0bae3d3f27677cb72
merryn <h1335@naver.com>
2020-04-23 22:58:25 +0900

    different way

--- Hash/hash_1.py
+++ Hash/hash_1.py
@@ -1,6 +1,6 @@
 # 문제 설명
 # 수많은 마라톤 선수들이 마라톤에 참여하였습니다.
-#  단 한 명의 선수를 제외하고는 모든 선수가 마라톤을 완주하였습니다.
+# 단 한 명의 선수를 제외하고는 모든 선수가 마라톤을 완주하였습니다.
 
 # 마라톤에 참여한 선수들의 이름이 담긴 배열 participant와 완주한 선수들의 이름이 담긴 배열 completion이 주어질 때, 완주하지 못한 선수의 이름을 return 하도록 solution 함수를 작성해주세요.
 
@@ -9,11 +9,13 @@
 # completion의 길이는 participant의 길이보다 1 작습니다.
 # 참가자의 이름은 1개 이상 20개 이하의 알파벳 소문자로 이루어져 있습니다.
 # 참가자 중에는 동명이인이 있을 수 있습니다.
+
 # 입출력 예
 # participant	completion	return
 # [leo, kiki, eden]	[eden, kiki]	leo
 # [marina, josipa, nikola, vinko, filipa]	[josipa, filipa, marina, nikola]	vinko
 # [mislav, stanko, mislav, ana]	[stanko, ana, mislav]	mislav
+
 # 입출력 예 설명
 # 예제 #1
 # leo는 참여자 명단에는 있지만, 완주자 명단에는 없기 때문에 완주하지 못했습니다.
@@ -40,4 +42,12 @@ def solution(participants, completion):
             answer = k
     return answer
 
-# collection counter
+# different algorithm
+
+
+def solution(participants, completion):
+    for c in completion:
+        if c in participants:
+            participants.remove(c)
+    answer = participants[0]
+    return answer
