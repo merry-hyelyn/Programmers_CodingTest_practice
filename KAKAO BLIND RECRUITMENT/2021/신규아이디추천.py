a9941b9d55f645722d1fb1a749697208e6155f38
Kimhyelyn <44598936+merry-hyelyn@users.noreply.github.com>
2022-06-23 01:48:38 +0900

    feat: 등호 하나때문에 계속 틀림

--- KAKAO BLIND RECRUITMENT/2021/신규아이디추천.py
+++ KAKAO BLIND RECRUITMENT/2021/신규아이디추천.py
@@ -1,45 +1,25 @@
-def step_two(new_id):
-    condition = ['-', '_', '.']
-    temp = new_id
-    for i in temp:
-        if not i.isdigit() and i not in condition and not i.isalpha():
-            new_id = new_id.replace(i, '')
-    return new_id
+from collections import defaultdict
 
 
-def step_three(new_id):
-    prev = ''
-    target = ''
-    temp = new_id
-    for w in temp:
-        if w == '.':
-            if prev:
-                print(prev)
-                if prev == w:
-                    target += prev
-                else:
-                    if target:
-                        target += prev
-                        print("target : ", target)
-                        new_id = new_id.replace(target, '.')
-                        target = ''
-        prev = w
-    return new_id
-
-
-def solution(new_id):
-    answer = ''
-    print('0 : ', new_id)
-
-    new_id = new_id.lower()
-    print("1 : ", new_id)
-
-    new_id = step_two(new_id)
-    print("2 : ", new_id)
-
-    new_id = step_three(new_id)
-    print("3 : ", new_id)
+def solution(id_list, report, k):
+    answer = []
+    reporters = defaultdict(set)
+    be_reported = defaultdict(set)
+    count_reported = defaultdict(int)
+    for user in report:
+        u, s = user.split()
+        reporters[u].add(s)
+        if u not in be_reported[s]:
+            be_reported[s].add(u)
+            count_reported[s] += 1
+    stopped_ids = []
+    for user, count in count_reported.items():
+        if count >= k:
+            stopped_ids.append(user)
+    for user in id_list:
+        count = 0
+        for stopped in stopped_ids:
+            if stopped in reporters[user]:
+                count += 1
+        answer.append(count)
     return answer
-
-
-solution("...!@BaT#*..y.abcdefghijklm")
